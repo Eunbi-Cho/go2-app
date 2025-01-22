3aff3a623e1af6b10de0a7749e727dc5c92be8dd
Eunbi Cho <moomb70@gmail.com>
2025-01-22 18:19:20 +0900

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
+import { getKeyHashAndroid, initializeKakaoSDK } from '@react-native-kakao/core';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import {login, logout, unlink} from '@react-native-kakao/user';
 
 export default function App() {
+  useEffect(() => {
+    initializeKakaoSDK('50bd8f2b2fe7234f97dab6d209339b61');
+  })
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title={'로그인'} onPress={() => {
+        login().then(console.log).catch(console.error)
+      }}/>
+      <Button title={'로그아웃'} onPress={() => {
+        logout().then(console.log).catch(console.error)
+      }}/>
+      <Button title={'회원탈퇴'} onPress={() => {
+        unlink().then(console.log).catch(console.error)
+      }}/>
       <StatusBar style="auto" />
     </View>
   );
